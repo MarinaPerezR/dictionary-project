bc5820903215b2dc5113c63b9570c873dcdaea38
MarinaPerezR <marina.perezrobles@gmail.com>
2025-01-08 21:12:03 +0000

    Fixed an error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import logo from "./logo.png";
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
+    <div className = "App">
+      <header className = "App-header">
+        <img src={logo} className = "App-logo img-fluid" alt="logo" />
       </header>
     </div>
   );
