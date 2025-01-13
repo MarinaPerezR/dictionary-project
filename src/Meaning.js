9a8a43be239b34ca55f9859d82228ff514bf8e82
MarinaPerezR <marina.perezrobles@gmail.com>
2025-01-13 15:08:52 +0000

    Created Example.js and Synonyms.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
+import Example from "./Example.js";
 
 export default function Meaning(props) {
     console.log(props.meaning);
     return (
         <div className="Meaning">
            <h3>{props.meaning.partOfSpeech}</h3>
-           <p>{props.meaning.definition}</p>
+           <p>
+              <strong>Definition: </strong>{props.meaning.definition}
+              <br />
+              <Example example={props.meaning.example} />
+              <br />
+              <Synonyms synonyms={props.meaning.synonyms} />
+            </p>
         </div>     
     );
 
